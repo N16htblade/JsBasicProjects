263592d2aec7214bab2986d5c1add7512c13849b
N16htblade <aritonpaul@gmail.com>
2020-12-07 19:01:43 -0500

    updated table and added working remove button

--- Js/expenseTracker.js
+++ Js/expenseTracker.js
@@ -1,9 +1,29 @@
+var myNodelist = document.getElementsByTagName("MyTable");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+
 function newExpense() {
 
     var name = document.getElementById("name").value;
     var date = document.getElementById("date").value;
     var amount = document.getElementById("amount").value;
-    var arrData = [name, date, amount];
+    var arrData = [name, date, amount, ""];
 
     if (name | date | amount === '') {
         alert("Empty field detected, please fill in all information!");
@@ -15,6 +35,20 @@ function newExpense() {
             td = tr.insertCell(i);
             td.appendChild(t)
             document.getElementById("myTable").appendChild(tr);
+        }
+    }
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    td.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            var p = this.parentNode.parentNode.rowIndex;
+            console.log("Row index is: " + p)
+            document.getElementById('myTable').deleteRow(p);
         }
     }
 }
