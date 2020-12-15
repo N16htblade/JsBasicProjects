a41f804c412bfa4e3fb418cc070dc489db10ec54
N16htblade <aritonpaul@gmail.com>
2020-12-14 21:12:19 -0500

    changed table to divs and js

--- Js/expenseTracker.js
+++ Js/expenseTracker.js
@@ -10,30 +10,40 @@ function newExpense() {
     if (name === '' || date == '' || amount === '') {
         alert("Empty field detected, please fill in all information!");
     } else {
-        var table = document.getElementById("myTable");
-        var tr = table.insertRow();
-        for (var i = 0; i < arrData.length; i++) {
-            var t = document.createTextNode(arrData[i]);
-            td = tr.insertCell(i);
-            td.appendChild(t)
-            document.getElementById("myTable").appendChild(tr);
+        v = document.createTextNode(arrData[0]);
+        var trow = document.createElement('div');
+        trow.className = 'trow';
+
+        var ncol = document.createElement('div');
+        ncol.className = 'tcol-50';
+        ncol.appendChild(v);
+        trow.appendChild(ncol);
+
+        for (var i = 1; i < arrData.length; i++) {
+            v = document.createTextNode(arrData[i]);
+            var tcol = document.createElement('div');
+            tcol.className = 'tcol-15';
+            tcol.appendChild(v);
+            trow.appendChild(tcol);
         }
+
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("Delete");
+        span.className = "close";
+        span.appendChild(txt);
+        tcol.appendChild(span);
+        trow.appendChild(tcol);
+
+        document.getElementById('myTbl').appendChild(trow);
     }
     document.getElementById("name").value = "";
     document.getElementById("date").value = "";
     document.getElementById("amount").value = "";
-
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("Delete");
-    span.className = "close";
-    span.appendChild(txt);
-    td.appendChild(span);
-
+    
     for (i = 0; i < close.length; i++) {
         close[i].onclick = function () {
-            var p = this.parentNode.parentNode.rowIndex;
-            console.log("Row index is: " + p)
-            document.getElementById('myTable').deleteRow(p);
+            var div = this.parentElement.parentElement;
+            div.style.display = "none";
         }
-    }
+    }    
 }
